fffffe16abf667782754f9a3c9bf28deffcf82d2
x0to1log <x0to1log@gmail.com>
2026-03-11 22:59:52 +0900

    feat: add blog post subcategory migration

---
+++ supabase/migrations/20260311_blog_post_subcategories.sql
@@ -0,0 +1,6 @@
+ALTER TABLE blog_posts
+DROP CONSTRAINT IF EXISTS blog_posts_category_check;
+
+ALTER TABLE blog_posts
+ADD CONSTRAINT blog_posts_category_check
+CHECK (category IN ('study', 'career', 'project', 'work-note', 'daily'));
